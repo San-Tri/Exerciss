b1293af6f31c08546a81e87ef745862eea92ec9f
San-Tri <stri80603@gmail.com>
2025-07-07 12:45:58 -0500

    ya funcionan la creacion de listas

--- To-Do_List/script.js
+++ To-Do_List/script.js
@@ -5,19 +5,35 @@ document.querySelector(".boton-nueva-tarea").addEventListener("click",
     let tarea = document.querySelector(".input-nueva-tarea").value;
     
     //verificamos si el campo esta vacio
-    if (tarea.trim() === "") {
-        console.log("el campo esta vacio")
-        alert("ingrese una tarea")
+    if (tarea.trim() === "") {//trim elimina los espacios vacios al inicio y final de la cadena
+        console.log("el campo esta vacio");
+        alert("ingrese una tarea");
     }else {
         //si el campo tiene contenido se guardara en lista de tareas
-        console.log("el campo tiene contenido")
+        console.log("el campo tiene contenido");
 
-        while (condition) {
-            
-        }
-        document.querySelector(".tarea").innerText= tarea;
-        alert("¡Nueva tarea creada!");
+        //creamos listas para tarea sin quese sobre escriba
+        let nuevaTarea = document.createElement("li") //creamos la etiqueta li
+        nuevaTarea.classList.add("n-tarea");// le agregamos una clase
+        nuevaTarea.innerHTML=`
+        <input type="checkbox" class="checkbox">
+        <p class="tarea"></p>
+        <button class="boton-eliminar tarea">Eliminar</button>`;// agregamos lo que incluira la etiqueta li
+        nuevaTarea.querySelector("p").innerText= tarea;//agregamos la nueva tarea en la etiqueta p
+        document.querySelector(".lista-tareas").appendChild(nuevaTarea);//agregamos todo esto dentro de la etiqueta ul
+        
 
-        document.querySelector(".input-nueva-tarea").value="";                
+        //limpia el input despues de guardar el anterior
+        document.querySelector(".input-nueva-tarea").value="";
+        
+        //agregamos diseño para que cuando le demos al checkbox se ponga roja la letra y este tachada
+        nuevaTarea.querySelector(".checkbox").addEventListener("click",
+            function() {
+                let tex_tarea = document.querySelector(".tarea")
+                tex_tarea.style.color = "red";
+                tex_tarea.style.textDecoration = "line-through";
+            }
+        );
     };
 });
+

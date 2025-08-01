4c0c47addf83dbfd6fc388d0af351b35d1372df7
San-Tri <stri80603@gmail.com>
2025-08-01 11:11:10 -0500

    ayuda

--- To-Do_List/script.js
+++ To-Do_List/script.js
@@ -1,3 +1,12 @@
+//funcion para cargar tareas del localStorage al iniciar la pagina
+window.addEventListener("DOMContentLoaded", function () {
+    let tareasGuardadas = JSON.parse(localStorage.getItem("tareas")) || [];
+
+    tareasGuardadas.forEach(function (texto) {
+        agregarTarea(texto);        
+    });
+});
+
 
 document.querySelector(".boton-nueva-tarea").addEventListener("click", 
     function() {
@@ -6,62 +15,64 @@ document.querySelector(".boton-nueva-tarea").addEventListener("click",
     
     //verificamos si el campo esta vacio
     if (tarea.trim() === "") {//trim elimina los espacios vacios al inicio y final de la cadena
-        console.log("el campo esta vacio");
         alert("ingrese una tarea");
     }else {
+        agregarTarea(tarea);
+        guardarTarea(tarea);
 
-        //si el campo tiene contenido se guardara en lista de tareas
-        console.log("el campo tiene contenido");
+    };
+});
 
-        //REGISTRAR TAREA
-
-        //creamos listas para tarea sin quese sobre escriba
-        let nuevaTarea = document.createElement("li") //creamos la etiqueta li
-        nuevaTarea.classList.add("n-tarea");// le agregamos una clase
-        nuevaTarea.innerHTML=`
+//funcion para agregar una tarea al DoM
+function agregarTarea(tarea) {
+    let nuevaTarea = document.createElement("li"); //creamos la etiqueta li
+    nuevaTarea.classList.add("n-tarea");// le agregamos una clase
+    nuevaTarea.innerHTML=`
         <input type="checkbox" class="checkbox">
         <p class="tarea"></p>
         <button class="boton-eliminar tarea">Eliminar</button>`;// agregamos lo que incluira la etiqueta li
-        nuevaTarea.querySelector("p").innerText= tarea;//agregamos la nueva tarea en la etiqueta p
-        document.querySelector(".lista-tareas").appendChild(nuevaTarea);//agregamos todo esto dentro de la etiqueta ul
-        
+    nuevaTarea.querySelector(".tarea").innerText= tarea;//agregamos la nueva tarea en la etiqueta p
+    document.querySelector(".lista-tareas").appendChild(nuevaTarea);//agregamos todo esto dentro de la etiqueta ul
 
-        //limpia el input despues de guardar el anterior
-        document.querySelector(".input-nueva-tarea").value="";
+    //limpia el input despues de guardar el anterior
+    document.querySelector(".input-nueva-tarea").value="";
+    
+    //MARCAR TAREA
+    
+    //agregamos diseño para que cuando le demos al checkbox 
+    nuevaTarea.querySelector(".checkbox").addEventListener("click",
+        function() {
+            
+            //obtenemos el valor del checkbox marcado o no
+            let checked = nuevaTarea.querySelector(".checkbox").checked
 
-        //MARCAR TAREA
-        
-        //agregamos diseño para que cuando le demos al checkbox 
-        nuevaTarea.querySelector(".checkbox").addEventListener("click",
-            function() {
-                
-                //obtenemos el valor del checkbox marcado o no
-                let checked = nuevaTarea.querySelector(".checkbox").checked
+            //otenemos el valor de la lista creada
+            let tex_tarea = nuevaTarea.querySelector(".tarea")
 
-                //otenemos el valor de la lista creada
-                let tex_tarea = nuevaTarea.querySelector(".tarea")
-
-                //verificamos si el checkbox esta marcado o no para aplicar el diseño
-                if (checked) {
-                    tex_tarea.style.color = "red";
-                    tex_tarea.style.textDecoration = "line-through";   
-                }else{
-                    tex_tarea.style.color = "black";
-                    tex_tarea.style.textDecoration = "none";
-                }
+            //verificamos si el checkbox esta marcado o no para aplicar el diseño
+            if (checked) {
+                tex_tarea.style.color = "red";
+                tex_tarea.style.textDecoration = "line-through";   
+            }else{
+                tex_tarea.style.color = "black";
+                tex_tarea.style.textDecoration = "none";
             }
-        );
+        }
+    );
+    
+    //ELIMINAR TAREA
 
-        //ELIMINAR TAREA
+    //funcionalidad para eliminar la tarea
+    nuevaTarea.querySelector(".boton-eliminar").addEventListener("click",
+        function() {
 
-        //funcionalidad para eliminar la tarea
-        nuevaTarea.querySelector(".boton-eliminar").addEventListener("click",
-            function() {
+            //seleccionamos al elemento padre y eliminamos a su hijo
+            nuevaTarea.parentElement.removeChild(nuevaTarea);
+        }
+    );
 
-                //seleccionamos al elemento padre y eliminamos a su hijo
-                nuevaTarea.parentElement.removeChild(nuevaTarea);
-            }
-        );
-    };
-});
+    //funcion para guardar nueva tarea al localStorage
+    function guardarTarea()
+
+}
 
